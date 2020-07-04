428ae0ebd6cf3927ffabf712197a80e207b3a775
Vincent Tshabalala <vincent.b.tshabalala@gmail.com>
2020-07-04 16:54:40 +0200

    Added functionality for making a move on the board'

--- test/pegthing/core_test.clj
+++ test/pegthing/core_test.clj
@@ -1,6 +1,26 @@
 (ns pegthing.core-test
   (:require [clojure.test :refer :all]
             [pegthing.core :refer :all]))
+(defn pegged?
+  "Does the position have a peg in it"
+  [board pos]
+  (get-in board [pos :pegged]))
+
+(defn remove-peg
+  "Take the peg at a given position out of the board"
+  [board pos]
+  (assoc-in board [pos :pegged] false))
+
+(defn place-peg
+  "Place a peg at a given position"
+  [board pos]
+  (assoc-in board [pos :pegged] true))
+
+(defn move-peg
+  "Take peg out initial-pos and move it to destination"
+  [board initial-pos destination]
+  (place-peg (remove-peg board initial-pos) destination))
+
 (deftest pegthing-test
   (testing "Tri function"
     (is (= '(1 3 6 10 15) (take 5 tri))))
@@ -54,8 +74,57 @@
             13 {:pegged true, :connections {15 14, 11 12, 6 9, 4 8}},
             14 {:pegged true, :connections {12 13, 5 9}},
             15 {:pegged true, :connections {13 14, 6 10}},
-            :rows 5} (new-board 5))))
-  (testing "Moving pegs"
-    (testing "is a position pegged?"
-      (defn board (new-board [15]))
-      (is (= true (pegged? board 1))))))
+            :rows 5} (new-board 5)))))
+
+(defn valid-moves
+  "Return a map of valid moves for pos, where the key is the destination and
+   the value is the jumped position e.g {4 2}"
+  [board pos]
+  (into {}
+        (filter (fn [[destination jumped]]
+                  (and (not (pegged? board destination))
+                       pegged? board jumped))
+                (get-in board [pos :connections])))
+  )
+
+(defn valid-move?
+  "Return jumped position if move is valid or return nil"
+  [board initial-pos destination]
+  (get (valid-moves board initial-pos) destination))
+
+(defn make-move
+  "Move peg from initial-pos to destination, removing jumped position"
+  [board initial-pos destination]
+  (if-let [jumped (valid-move? board initial-pos destination)]
+    (move-peg (remove-peg board jumped) initial-pos destination)))
+
+(deftest moving-pegs
+  (testing "Is a position pegged?"
+    (let [board (new-board 15)]
+      (is (= true (pegged? board 1)))))
+  (testing "Remove a peg from a position"
+    (let [board (new-board 15)]
+      (is (= false (pegged? (remove-peg board 1) 1)))))
+  (testing "Place a peg at a position"
+    (let [board (remove-peg (new-board 15) 4)]
+      (is (= true (pegged? (place-peg board 4) 4)))))
+  (testing "Move a peg from one position to another"
+    (let [board (remove-peg (new-board 15) 4)
+          new-board (move-peg board 1 4)]
+      (is (= false (pegged? new-board 1)))
+      (is (= true (pegged? new-board 4)))))
+  (testing "Valid moves on the board"
+    (let [board (new-board 15)]
+         (is (= {} (valid-moves board 1)))
+         (is (= {4 2} (valid-moves (remove-peg board 4) 1)))
+         (is (= {4 2 6 3} (valid-moves (remove-peg (remove-peg board 4) 6) 1)))))
+  (testing "whether a move is valid"
+    (let [board (remove-peg (new-board 15) 4) ]
+      (is (= 2 (valid-move? board 1 4)))
+      (is (= nil (valid-move? board 8 4)))))
+  (testing "making a move"
+    (let [board (remove-peg (new-board 15) 4)
+          new-board (make-move  board 1 4)]
+      (is (= true (pegged? new-board 4)))
+      (is (= false (pegged? new-board 1)))
+      (is (= false (pegged? new-board 2))))))
